038f236638d3f84184326ed6ec5da71f1cc928af
Travis Reed <travisreed.dfw@gmail.com>
2023-10-26 14:03:14 -0500

    Update file.ps1

--- file.ps1
+++ file.ps1
@@ -2,88 +2,58 @@
 $columnName1 = "ControlHeader"
 $columnName2 = "ControlHeader2"
 
-# Get all CSV and Excel files in the current directory
-$filePaths = Get-ChildItem -Filter '{*.csv,*.xlsx}'
+# Get all CSV files in the current directory
+$csvFiles = Get-ChildItem -Filter *.csv
 
-# Process each file
-foreach ($filePath in $filePaths) {
+# Function to detect file encoding
+function Get-FileEncoding {
+    param([string]$path)
+    
+    [byte[]]$byte = get-content -Encoding byte -ReadCount 4 -TotalCount 4 -Path $path
+    
+    if ($byte[0] -eq 0x2b -and $byte[1] -eq 0x2f -and $byte[2] -eq 0x76) { return New-Object System.Text.UTF7Encoding }
+    if ($byte[0] -eq 0xef -and $byte[1] -eq 0xbb -and $byte[2] -eq 0xbf) { return New-Object System.Text.UTF8Encoding }
+    if ($byte[0] -eq 0xff -and $byte[1] -eq 0xfe) { return New-Object System.Text.UnicodeEncoding }
+    if ($byte[0] -eq 0xfe -and $byte[1] -eq 0xff) { return New-Object System.Text.BigEndianUnicodeEncoding }
+    if ($byte[0] -eq 0 -and $byte[1] -eq 0 -and $byte[2] -eq 0xfe -and $byte[3] -eq 0xff) { return New-Object System.Text.UTF32Encoding }
+    
+    return New-Object System.Text.ASCIIEncoding
+}
+
+# Process each CSV file
+foreach ($csvPath in $csvFiles) {
 
     # Check if the file name matches the date pattern and skip if it does
-    if ($filePath.Name -match "_\d{8}_\d{4}\.") {
-        Write-Output "Skipping file: $($filePath.Name) as it seems to have been processed before."
+    if ($csvPath.Name -match "_\d{8}_\d{4}\.csv$") {
+        Write-Output "Skipping file: $($csvPath.Name) as it seems to have been processed before."
         continue
     }
 
-    # Variable to hold data
-    $data = @()
+    # Detect the encoding of the file
+    $encoding = Get-FileEncoding -Path $csvPath.FullName
+    
+    # Read the CSV file and replace spaces using the detected encoding
+    $content = Get-Content -Path $csvPath.FullName -Raw -Encoding $encoding.EncodingName
+    $noSpacesContent = $content -replace ' ', ''
 
-    # Import the file based on its type
-    if ($filePath.Extension -eq '.csv') {
-        $data = Import-Csv -Path $filePath.FullName
-    } elseif ($filePath.Extension -eq '.xlsx') {
-        # Create a new Excel application object
-        $excel = New-Object -ComObject Excel.Application
-        $excel.Visible = $false
-        $workbook = $excel.Workbooks.Open($filePath.FullName)
-        $worksheet = $workbook.Worksheets.Item(1)
-        $range = $worksheet.UsedRange
-        $data = $range.Value2 | ConvertTo-Csv -Delimiter "," -NoTypeInformation | ConvertFrom-Csv
+    # Convert the string content back to CSV object for structured manipulation
+    $csvData = $noSpacesContent | ConvertFrom-Csv
 
-        # Close Excel without saving and release COM objects
-        $excel.Quit()
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet) | Out-Null
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
-        [System.GC]::Collect()
-        [System.GC]::WaitForPendingFinalizers()
-    } else {
-        Write-Warning "Unsupported file type for $($filePath.Name)"
-        continue
-    }
-
-    # Remove spaces and update columns with comma replacements
-    foreach ($row in $data) {
-        foreach ($property in $row.PSObject.Properties) {
-            # Remove spaces
-            $property.Value = $property.Value -replace ' ', ''
-
-            # Replace commas for the specified columns
-            if ($property.Name -eq $columnName1 -or $property.Name -eq $columnName2) {
-                $property.Value = $property.Value -replace ',', ', '
-            }
+    # Update the specified columns
+    foreach ($row in $csvData) {
+        if ($row.PSObject.Properties.Name -contains $columnName1) {
+            $row.$columnName1 = $row.$columnName1 -replace ',', ', '
+        }
+        if ($row.PSObject.Properties.Name -contains $columnName2) {
+            $row.$columnName2 = $row.$columnName2 -replace ',', ', '
         }
     }
 
     # Generate the new filename with date and time appended
     $currentDate = Get-Date -Format "yyyyMMdd_HHmm"
-    $newFileName = "$($filePath.BaseName)_$currentDate$($filePath.Extension)"
-    $newFilePath = Join-Path -Path $filePath.DirectoryName -ChildPath $newFileName
+    $newFileName = "$($csvPath.BaseName)_$currentDate.csv"
+    $newFilePath = Join-Path -Path $csvPath.DirectoryName -ChildPath $newFileName
 
-    # Save the modified content back based on its type
-    if ($filePath.Extension -eq '.csv') {
-        $data | Export-Csv -Path $newFilePath -NoTypeInformation -Delimiter ","
-    } elseif ($filePath.Extension -eq '.xlsx') {
-        # Open Excel again for saving data
-        $excel = New-Object -ComObject Excel.Application
-        $excel.Visible = $false
-        $workbook = $excel.Workbooks.Add()
-        $worksheet = $workbook.Worksheets.Item(1)
-        $worksheet.Name = "Processed Data"
-        
-        # Convert data to a format suitable for Excel and populate the worksheet
-        $dataArray = $data | ConvertTo-Csv -Delimiter "," -NoTypeInformation | ConvertFrom-Json
-        $worksheet.Range("A1").Resize($dataArray.Length, $dataArray[0].Length).Value = $dataArray
-
-        # Save the workbook
-        $workbook.SaveAs($newFilePath)
-        $workbook.Close($true)
-        $excel.Quit()
-        
-        # Release COM objects again
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet) | Out-Null
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
-        [System.GC]::Collect()
-        [System.GC]::WaitForPendingFinalizers()
-    }
+    # Save the modified content to the new CSV file in UTF-8
+    $csvData | Export-Csv -Path $newFilePath -NoTypeInformation -Encoding UTF8
 }
