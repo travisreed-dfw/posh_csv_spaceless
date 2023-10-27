d0d476df740b28aa153b9242d76b994490ce5ff8
Travis Reed <travisreed.dfw@gmail.com>
2023-10-27 14:23:23 -0500

    Update file.ps1

--- file.ps1
+++ file.ps1
@@ -2,48 +2,52 @@
 $columnName1 = "ControlHeader"
 $columnName2 = "ControlHeader2"
 
-# Get all CSV files in the current directory
-$csvFiles = Get-ChildItem -Filter *.csv
+# Get all CSV files in the current directory excluding already processed ones
+$csvFiles = Get-ChildItem -Filter *.csv | Where-Object { $_.Name -notmatch "_\d{8}_\d{4}\.csv$" }
 
 # Process each CSV file
 foreach ($csvPath in $csvFiles) {
+    try {
+        # Import the CSV
+        $data = Import-Csv $csvPath.FullName -Encoding Default
 
-    # Check if the file name matches the date pattern and skip if it does
-    if ($csvPath.Name -match "_\d{8}_\d{4}\.csv$") {
-        Write-Output "Skipping file: $($csvPath.Name) as it seems to have been processed before."
-        continue
+        # Skip if the file is empty
+        if (-not $data) {
+            Write-Host "Skipping empty file: $($csvPath.Name)"
+            continue
+        }
+
+        # Process each row to trim spaces and then update specified columns
+        foreach ($row in $data) {
+            # Trim spaces from all columns
+            foreach ($property in $row.PSObject.Properties) {
+                $row.$property.Name = $row.$property.Name.Trim()
+            }
+
+            # Update the specified columns
+            if ($columnName1 -in $row.PSObject.Properties.Name) {
+                $row.$columnName1 = $row.$columnName1 -replace ',', ', '
+            }
+            if ($columnName2 -in $row.PSObject.Properties.Name) {
+                $row.$columnName2 = $row.$columnName2 -replace ',', ', '
+            }
+        }
+
+        # Backup original file
+        #Copy-Item -Path $csvPath.FullName -Destination "$($csvPath.FullName).backup"
+
+        # Generate the new filename with date and time appended
+        $currentDate = Get-Date -Format "yyyyMMdd_HHmm"
+        $newFileName = "$($csvPath.BaseName)_$currentDate.csv"
+        $newFilePath = Join-Path -Path $csvPath.DirectoryName -ChildPath $newFileName
+
+        # Export the processed data to the new file
+        $data | Export-Csv $newFilePath -NoTypeInformation -Encoding UTF8
+
+        Write-Host "File saved to: $newFilePath"
+    } catch {
+        $errorMsg = "An error occurred processing $($csvPath.Name): $_"
+        Write-Error $errorMsg
+        Add-Content -Path "errorlog.txt" -Value $errorMsg
     }
-
-    # Import the CSV
-    $data = Import-Csv $csvPath.FullName
-
-    # Process each row and cell to remove trailing spaces and update specified columns
-    $processedData = $data | ForEach-Object {
-        $row = $_
-
-        # Update the specified columns
-        if ($row.PSObject.Properties.Name -contains $columnName1) {
-            $row.$columnName1 = ($row.$columnName1 -replace ',', ', ').TrimEnd()
-        }
-        if ($row.PSObject.Properties.Name -contains $columnName2) {
-            $row.$columnName2 = ($row.$columnName2 -replace ',', ', ').TrimEnd()
-        }
-
-        # Trim trailing spaces from all columns
-        $row.PsObject.Properties | ForEach-Object {
-            $row.$($_.Name) = $row.$($_.Name).TrimEnd()
-        }
-
-        return $row
-    }
-
-    # Generate the new filename with date and time appended
-    $currentDate = Get-Date -Format "yyyyMMdd_HHmm"
-    $newFileName = "$($csvPath.BaseName)_$currentDate.csv"
-    $newFilePath = Join-Path -Path $csvPath.DirectoryName -ChildPath $newFileName
-
-    # Export the processed data to the new file
-    $processedData | Export-Csv $newFilePath -NoTypeInformation -Encoding Default
-
-    Write-Output "File saved to: $newFilePath"
 }
